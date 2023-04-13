67f13a8749959c624cf62bf325aa89e8ba02d3b8
三月 <327750124@qq.com>
2023-04-13 16:07:20 +0800

    自动化设置skynet环境

--- run.sh
+++ run.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
+# 检查是否安装了git
 if ! dpkg-query -W -f='${Status}' git 2>/dev/null | grep -q "ok installed"; then
-    echo "Error : git is not installed."
-    exit 1
+    echo "instatll git ..."
+    sudo apt-get install git
 fi
+git --version
 
-echo "Find git continue!"
+# 检查是否安装了autoconf
+if ! dpkg-query -W -f='${Status}' autoconf 2>/dev/null | grep -q "ok installed"; then
+    echo "instatll autoconf ..."
+    sudo apt-get install autoconf
+fi
+autoconf --version
+
+# 检查是否安装了gcc
+if ! dpkg-query -W -f='${Status}' gcc 2>/dev/null | grep -q "ok installed"; then
+    echo "instatll gcc ..."
+    sudo apt-get install gcc
+fi
+gcc --version
+
+# 下载skynet源码
+echo "clone skynet ..."
+git clone http:://gitee.com/mirrors/skynet/git
+
+cd skynet
+
+# 编译skynet
+make linux
+
+# 运行案例
+./skynet example/config
