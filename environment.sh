7aeebcbdec9a14f60fdce15f05ee6e611becc23f
三月 <327750124@qq.com>
2023-04-13 16:31:27 +0800

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -7,12 +7,38 @@
 #fi
 #git --version
 
+function pause(){
+        echo 'Press any key to continue...'
+        read -n 1 -p "$*" str_inp
+        if [ -z "$str_inp" ];then
+                str_inp=1
+        fi
+        #echo "+$str_inp+"
+        if [ $str_inp != '' ] ; then
+                echo -ne '\b \n'
+        fi
+}
+
+# 检查是否安装了mysql
+if ! dpkg-query -W -f='${Status}' mysql 2>/dev/null | grep -q "ok installed"; then
+    echo "instatll mysql ..."
+    sudo apt-get install mysql-server
+fi
+mysql --version
+
+echo "start mysql"
+service mysql start
+ps -axj |grep mysql
+#service mysql stop
+pause
+
 # 检查是否安装了autoconf
 if ! dpkg-query -W -f='${Status}' autoconf 2>/dev/null | grep -q "ok installed"; then
     echo "instatll autoconf ..."
     sudo apt-get install autoconf
 fi
 autoconf --version
+pause
 
 # 检查是否安装了gcc
 if ! dpkg-query -W -f='${Status}' gcc 2>/dev/null | grep -q "ok installed"; then
@@ -20,10 +46,12 @@ if ! dpkg-query -W -f='${Status}' gcc 2>/dev/null | grep -q "ok installed"; then
     sudo apt-get install gcc
 fi
 gcc --version
+pause
 
 SKYNET_FILE=~/skynet
 if ! [ -d "$SKYNET_FILE" ]; then
     # 下载skynet源码
     echo "clone skynet ..."
     git clone http:://gitee.com/mirrors/skynet.git
+    pause
 fi
