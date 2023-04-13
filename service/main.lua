f0d3d4ba28d564b2505382847255206072d20048
三月 <327750124@qq.com>
2023-04-13 18:39:15 +0800

    test mysql

--- service/main.lua
+++ service/main.lua
@@ -1,8 +1,28 @@
 
 local skynet = require "skynet"
+local mysql = require "skynet.db.mysql"
 
 skynet.start(function ()
     skynet.error("[Start Main] Server Start....")
+
+    local db = mysql.connet(
+        {
+            host = "127.0.0.1",
+            port = 3306,
+            database = "test_db",
+            user = "root",
+            password = "123456",
+            max_packet_size = 1024 * 1024,
+            on_connet = nil,
+        }
+    )
+
+    local res = db:query("insert into users(name) values (\'aaaa\')")
+
+    res = db:query('select * form users')
+    for k, v in pairs(res) do
+        print(k .. " " .. v.id .. "  " .. v.name)
+    end
 
     local worker = skynet.newservice("worker", 1, "worker")
     local buy = skynet.newservice("buy", 1, "buy")
