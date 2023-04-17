fcd20f07e96474562a38b1c09e2c55bcf4d831b7
三月 <327750124@qq.com>
2023-04-17 16:13:41 +0800

    服务器设置+test db

--- service/main.lua
+++ service/main.lua
@@ -9,7 +9,7 @@ skynet.start(function ()
         {
             host = "127.0.0.1",
             port = 3306,
-            database = "test_db",
+            database = "login_system",
             user = "root",
             password = "123456",
             max_packet_size = 1024 * 1024,
@@ -17,11 +17,11 @@ skynet.start(function ()
         }
     )
 
-    local res = db:query("insert into users(name) values (\'aaaa\')")
+    local res = db:query("insert into user(username,) values (\'aaaa\')")
 
-    res = db:query('select * form users')
+    res = db:query('select * form user')
     for k, v in pairs(res) do
-        print(k .. " " .. v.id .. "  " .. v.name)
+        print(k .. " " .. v.id .. "  " .. v.username)
     end
 
     local worker = skynet.newservice("worker", 1, "worker")
