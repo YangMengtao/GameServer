238c21fd58dab9ea4f0c18433a46cde5b7820d32
三月 <327750124@qq.com>
2023-04-23 17:14:35 +0800

    test

---
+++ lualib/WebInterface.lua
@@ -0,0 +1,40 @@
+local skynet = require "skynet"
+local sockethelper = require "http.sockethelper"
+
+local WebInterface = {}
+
+WebInterface.SSLCTX_SERVER = nil
+
+WebInterface.Gen = function (protocol, fd)
+    if protocol == "http" then
+		return {
+			init = nil,
+			close = nil,
+			read = sockethelper.readfunc(fd),
+			write = sockethelper.writefunc(fd),
+		}
+	elseif protocol == "https" then
+		local tls = require "http.tlshelper"
+		if not WebInterface.SSLCTX_SERVER then
+			WebInterface.SSLCTX_SERVER = tls.newctx()
+			-- gen cert and key
+			-- openssl req -x509 -newkey rsa:2048 -days 3650 -nodes -keyout server-key.pem -out server-cert.pem
+			local certfile = skynet.getenv("certfile") or "./server-cert.pem"
+			local keyfile = skynet.getenv("keyfile") or "./server-key.pem"
+			print(certfile, keyfile)
+			WebInterface.SSLCTX_SERVER:set_cert(certfile, keyfile)
+		end
+		local tls_ctx = tls.newtls("server", WebInterface.SSLCTX_SERVER)
+		return {
+			init = tls.init_responsefunc(fd, tls_ctx),
+			close = tls.closefunc(tls_ctx),
+			read = tls.readfunc(fd, tls_ctx),
+			write = tls.writefunc(fd, tls_ctx),
+		}
+	else
+		error(string.format("Invalid protocol: %s", protocol))
+        return nil
+	end
+end
+
+return WebInterface
--- service/main.lua
+++ service/main.lua
@@ -1,24 +1,91 @@
 local skynet = require "skynet"
-require "skynet.manager" -- 引入 skynet.manager 模块
+local socket = require "skynet.socket"
+local httpd = require "http.httpd"
+local sockethelper = require "http.sockethelper"
+local urllib = require "http.url"
+local webInterface = require "WebInterface"
+local cjson = require "cjson"
 
-skynet.start(function()
-    skynet.start(function()
-        skynet.newservice("socket")
-        local httpd = skynet.newservice("httpd")
-        skynet.call(httpd, "lua", "start", {
-            port = 3636,
-            accesslog = skynet.getenv("accesslog") or "access.log",
-            autoindex = true,
-            root = "./www"
-        })
-    
-        skynet.dispatch("http", function(session, address, method, header, url, body)
-            local respheader = {
-                ["Content-Type"] = "text/html",
-            }
-            local respbody = "<html><head><title>Hello Skynet</title></head><body><h1>Hello Skynet!</h1></body></html>"
-            local status = 200
-            skynet.ret(skynet.pack(status, respheader, respbody))
-        end)
+local mode, protocol = ...
+protocol = protocol or "http"
+
+local function response(id, write, ...)
+	local ok, err = httpd.write_response(write, ...)
+	if not ok then
+		-- if err == sockethelper.socket_error , that means socket closed.
+		skynet.error(string.format("fd = %d, %s", id, err))
+	end
+end
+
+local function request()
+    skynet.dispatch("lua", function (source, session, fd)
+        socket.start(fd)
+
+        local wi = webInterface.Gen(protocol, fd)
+        if wi then
+            if wi.init then
+                wi.init()
+            end
+            local code, url, method, header, body = httpd.read_request(wi.read)
+            if code then
+                if code ~= 200 then
+                    response(fd, wi.write, code)
+                else
+                    local path, query = urllib.parse(url)
+                    local tmp = "{ code = " .. code 
+                    if query then
+                        local q = urllib.parse_query(query)
+                        for k, v in pairs(q) do
+                            tmp = tmp .. ", " .. k .. "=" .. v
+                        end
+                        
+                    end
+                    tmp = "}"
+                    response(fd, wi.write, code, tmp)
+                end
+            else
+                if url == sockethelper.socket_error then
+                    skynet.error("[Error]:socket closed")
+                else
+                    skynet.error("[Error]:" .. url)
+                end
+            end
+        end
+
+        socket.close(fd)
+
+        if wi and wi.close then
+            wi.close()
+        end
     end)
+end
+
+-- 初始化web socket
+local function init()
+    local agent = {}
+    -- 创建20个服务处理不同客户端发来的消息
+    for i = 1, 20 do
+        agent[i] = skynet.newservice(SERVICE_NAME, "agent", protocol)
+    end
+
+    local balance = 1
+    local port = 3636
+	local id = socket.listen("0.0.0.0", port)
+
+    skynet.error(string.format("listen web port = [%s] protocol = [%s]", port, protocol))
+
+    socket.start(id, function (id, addr)
+        skynet.error(string.format("%s connected, pass it to agent :%08x", addr, agent[balance]))
+        skynet.send(agent[balance], "lua", id)
+		balance = balance + 1
+		if balance > #agent then
+			balance = 1
+		end
+    end)
+
+    request()
+end
+
+skynet.start(function ()
+    init()
 end)
