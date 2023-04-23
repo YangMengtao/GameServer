6bab9f78f4f1a3d890e549336100b6781149d4c6
三月 <327750124@qq.com>
2023-04-23 17:26:37 +0800

    test

---
+++ service/agent.lua
@@ -0,0 +1,57 @@
+local skynet = require "skynet"
+local socket = require "skynet.socket"
+local httpd = require "http.httpd"
+local sockethelper = require "http.sockethelper"
+local urllib = require "http.url"
+local webInterface = require "WebInterface"
+
+local protocol = "http"
+
+local function response(id, write, ...)
+	local ok, err = httpd.write_response(write, ...)
+	if not ok then
+		-- if err == sockethelper.socket_error , that means socket closed.
+		skynet.error(string.format("fd = %d, %s", id, err))
+	end
+end
+
+-- message callback
+skynet.start(skynet.dispatch("lua", function (source, session, fd)
+    socket.start(fd)
+
+    local wi = webInterface.Gen(protocol, fd)
+    if wi then
+        if wi.init then
+            wi.init()
+        end
+        local code, url, method, header, body = httpd.read_request(wi.read)
+        if code then
+            if code ~= 200 then
+                response(fd, wi.write, code)
+            else
+                local path, query = urllib.parse(url)
+                local tmp = "{ code = " .. code 
+                if query then
+                    local q = urllib.parse_query(query)
+                    for k, v in pairs(q) do
+                        tmp = tmp .. ", " .. k .. "=" .. v
+                    end
+                end
+                tmp = "}"
+                response(fd, wi.write, code, tmp)
+            end
+        else
+            if url == sockethelper.socket_error then
+                skynet.error("[Error]:socket closed")
+            else
+                skynet.error("[Error]:" .. url)
+            end
+        end
+    end
+
+    socket.close(fd)
+
+    if wi and wi.close then
+        wi.close()
+    end
+end))
--- service/main.lua
+++ service/main.lua
@@ -1,63 +1,8 @@
 local skynet = require "skynet"
 local socket = require "skynet.socket"
-local httpd = require "http.httpd"
-local sockethelper = require "http.sockethelper"
-local urllib = require "http.url"
-local webInterface = require "WebInterface"
 --local cjson = require "cjson"
 
-local mode, protocol = ...
-protocol = protocol or "http"
-
-local function response(id, write, ...)
-	local ok, err = httpd.write_response(write, ...)
-	if not ok then
-		-- if err == sockethelper.socket_error , that means socket closed.
-		skynet.error(string.format("fd = %d, %s", id, err))
-	end
-end
-
--- message callback
-skynet.start(skynet.dispatch("lua", function (source, session, fd)
-    socket.start(fd)
-
-    local wi = webInterface.Gen(protocol, fd)
-    if wi then
-        if wi.init then
-            wi.init()
-        end
-        local code, url, method, header, body = httpd.read_request(wi.read)
-        if code then
-            if code ~= 200 then
-                response(fd, wi.write, code)
-            else
-                local path, query = urllib.parse(url)
-                local tmp = "{ code = " .. code 
-                if query then
-                    local q = urllib.parse_query(query)
-                    for k, v in pairs(q) do
-                        tmp = tmp .. ", " .. k .. "=" .. v
-                    end
-                    
-                end
-                tmp = "}"
-                response(fd, wi.write, code, tmp)
-            end
-        else
-            if url == sockethelper.socket_error then
-                skynet.error("[Error]:socket closed")
-            else
-                skynet.error("[Error]:" .. url)
-            end
-        end
-    end
-
-    socket.close(fd)
-
-    if wi and wi.close then
-        wi.close()
-    end
-end))
+local protocol = "http"
 
 -- 初始化web socket
 skynet.start(function ()
