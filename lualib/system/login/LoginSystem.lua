496100c0d973f5b598ce8e1fa1c305cb8eba6a22
三月 <327750124@qq.com>
2023-04-26 16:59:05 +0800

    Update LoginSystem.lua

--- lualib/system/login/LoginSystem.lua
+++ lualib/system/login/LoginSystem.lua
@@ -44,8 +44,8 @@ function LoginSystem:login(data)
     if result then
         if #result > 0 then
             local uid = result[1].id
-            local password = md5.sumhexa(result[1].password)
-            if password == tostring(data.password) then
+            local password = result[1].password
+            if md5.sumhexa(password) == tostring(data.password) then
                 local token = self:getToken(uid)
                 self.m_UserList[token] = uid
                 ret.token = token
