21fca3fa43bbe6437538564e77e38b67dd3dc145
三月 <327750124@qq.com>
2023-04-27 17:18:31 +0800

    Update ServerManager.lua

--- lualib/ServerManager.lua
+++ lualib/ServerManager.lua
@@ -1,5 +1,6 @@
 local skynet = require "skynet"
 local Class = require "common.class"
+local errcode = require "common.errcode"
 
 local ServerManager = Class:new()
 
@@ -13,19 +14,14 @@ function ServerManager:addSystem(name, service)
 end
 
 function ServerManager:call(cmd, api, data)
-    if "lgoin" == cmd then
-        local service = self.m_Systems[cmd]
-        if service ~= nil then
-            local ret = skynet.call(service, "lua", api, data)
-            return ret
-        else
-            skynet.error("[System Error] : not found service = " .. cmd)
-        end
+    local service = self.m_Systems[cmd]
+    if service ~= nil then
+        local ret = skynet.call(service, "lua", api, data)
+        return ret
     else
-        
+        skynet.error("[System Error] : not found service = " .. cmd)
+        return { errcode = errcode.UNKNOWN}
     end
-    
-    return { errcode = -6666}
 end
 
 return ServerManager
