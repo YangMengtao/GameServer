8dadde64effb140f73cfd77210d3890ac831c9d5
三月 <327750124@qq.com>
2023-05-09 16:09:10 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 pid=$(cat skynet.pid)
+echo $pid
+kill -9 $pid
 
-kill -9 pid
-
-pid=$(ps aux | grep check_log_file_size.sh)
-
-kill -9 pid
+pid=$(ps -ef | grep check_log_file_size.sh | awk '{print $2}')
+echo $pid
+kill -9 $pid
 
 # 备份被删除原有的log文件
 file_path="logs/skynet.log"
