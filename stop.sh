4228ec093aeff0d3371ea37c20fa5e6c122d24f5
三月 <327750124@qq.com>
2023-05-09 16:28:12 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -8,7 +8,7 @@ pid=$(ps -ef | grep check_log_file_size.sh | awk '{print $2}')
 echo $pid
 kill -9 $pid
 
-# 备份被删除原有的log文件
+# 备份并删除原有的log文件
 file_path="logs/skynet.log"
 timestamp=$(date +%Y%m%d%H%M%S)
 cp "${file_path}" "${timestamp}.log"
